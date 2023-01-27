36d50adbe616a22641ada703e72729a1c74efdd4
Solomon Kebede <solomonaau188@gmail.com>
2023-01-27 20:06:27 +0000

    Task 4. MRU Caching

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+
+'''
+MRU Caching in python
+'''
+
+
+from collections import deque
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    '''LIFO Cache'''
+    def __init__(self):
+        '''Initialize cache'''
+        super().__init__()
+        self.count = 0
+        self.store1 = {}
+        self.store2 = {}
+
+    def put(self, key, item):
+        '''Put into cache'''
+        if key is not None and item is not None:
+            # print(self.cache_data)
+            if len(self.cache_data) < BaseCaching.MAX_ITEMS:
+                self.store1[key] = self.count
+                self.store2[self.count] = key
+                self.count += 1
+                self.cache_data[key] = item
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if key not in self.store1:
+                    mru_key = self.store2.get(max(self.store2.keys()))
+                    del self.cache_data[mru_key]
+                    print(f'DISCARD: {mru_key}')
+                    mru_key_value = self.store1[mru_key]
+                    del self.store1[mru_key]
+                    del self.store2[mru_key_value]
+                    self.store1[key] = self.count
+                    self.store2[self.count] = key
+                    self.count += 1
+                    self.cache_data[key] = item
+                else:
+                    # self.queue.append(key)
+                    key_value = self.store1[key]
+                    del self.store1[key]
+                    del self.store2[key_value]
+                    self.store1[key] = self.count
+                    self.store2[self.count] = key
+                    self.count += 1
+                    self.cache_data[key] = item
+
+    def get(self, key):
+        '''Get from cache'''
+        if key is None:
+            return None
+        return self.cache_data.get(key)
