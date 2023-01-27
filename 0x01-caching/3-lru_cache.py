80372896394b4d53aef96b2792675a525e083e6e
Solomon Kebede <solomonaau188@gmail.com>
2023-01-27 21:42:29 +0000

    Task 3. LRU Caching: Potential Fix

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -5,49 +5,41 @@
 LRU Caching in python
 '''
 
-
-from collections import deque
-
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    '''LRU Cache'''
+    '''
+    LRU Cache
+    '''
     def __init__(self):
         '''Initialize cache'''
         super().__init__()
-        self.count = 0
         self.store1 = {}
-        self.store2 = {}
+        self.count = 0
 
     def put(self, key, item):
         '''Put into cache'''
         if key is not None and item is not None:
-            # print(self.cache_data)
             if len(self.cache_data) < BaseCaching.MAX_ITEMS:
                 self.store1[key] = self.count
-                self.store2[self.count] = key
                 self.count += 1
                 self.cache_data[key] = item
             elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 if key not in self.store1:
-                    lru_key = self.store2.get(min(self.store2.keys()))
+                    lru_val = min(self.store1.values())
+                    lru_key = list(
+                        self.store1.keys()
+                        )[list(self.store1.values()).index(lru_val)]
                     del self.cache_data[lru_key]
                     print(f'DISCARD: {lru_key}')
-                    lru_key_value = self.store1[lru_key]
                     del self.store1[lru_key]
-                    del self.store2[lru_key_value]
                     self.store1[key] = self.count
-                    self.store2[self.count] = key
                     self.count += 1
                     self.cache_data[key] = item
                 else:
-                    # self.queue.append(key)
-                    key_value = self.store1[key]
-                    del self.store1[key]
-                    del self.store2[key_value]
+                    pass
                     self.store1[key] = self.count
-                    self.store2[self.count] = key
                     self.count += 1
                     self.cache_data[key] = item
 
@@ -55,4 +47,7 @@ class LRUCache(BaseCaching):
         '''Get from cache'''
         if key is None:
             return None
+        if key in self.store1.keys():
+            self.store1[key] = self.count
+            self.count += 1
         return self.cache_data.get(key)
