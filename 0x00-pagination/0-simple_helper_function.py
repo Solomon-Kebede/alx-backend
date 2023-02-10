b199e1d14021a8dbc003ef3c6b23677c380285af
Solomon Kebede <solomonaau188@gmail.com>
2023-02-10 19:31:12 +0000

    Task 0 - Pycode fix

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -8,10 +8,10 @@ for those particular pagination parameters.
 
 
 def index_range(page: int, page_size: int) -> tuple:
-	"""
-	Return a tuple of size two containing a
-	start index and an end index corresponding
-	to the range of indexes to return in a list
-	for those particular pagination parameters.
-	"""
-	return (page_size * (page-1), page_size * page)
+    """
+    Return a tuple of size two containing a
+    start index and an end index corresponding
+    to the range of indexes to return in a list
+    for those particular pagination parameters.
+    """
+    return (page_size * (page-1), page_size * page)
