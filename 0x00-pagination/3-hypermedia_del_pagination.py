e48281b3c274173e36c17e123c8463058f86364c
Solomon Kebede <solomonaau188@gmail.com>
2023-02-11 14:04:31 +0000

    Task 3 - partial implemention of get_hyper_index

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
-Deletion-resilient hypermedia pagination
+3. Deletion-resilient hypermedia pagination
 """
 
 import csv
 import math
-from typing import List
+from typing import List, Dict
+import math
 
 
 class Server:
@@ -39,5 +40,40 @@ class Server:
             }
         return self.__indexed_dataset
 
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns paginated page data depending on page and page_size"""
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        indice = index_range(page, page_size)
+        return self.dataset()[indice[0]:indice[1]]
+
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            pass
+        """Handle if between two queries, certain rows are removed
+        from the dataset, the user does not miss items from dataset
+        when changing page
+        """
+        # print('='*100)
+        if index is not None:
+            assert index < len(self.__dataset)
+            if index + 1 < len(self.__dataset):
+                next_index = index + 1
+            page = math.ceil(index/page_size)
+            # print(f"page={page}")
+        total_pages = math.ceil(len(self.dataset())/page_size)
+        # print(f"page_size = {page_size}, index = {index}")
+        return {
+            "index": index,
+            "next_index": next_index,
+            "page_size": page_size,
+            "data": self.get_page(page, page_size),
+        }
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Return a tuple of size two containing a
+    start index and an end index corresponding
+    to the range of indexes to return in a list
+    for those particular pagination parameters.
+    """
+    return (page_size * (page-1), page_size * page)
