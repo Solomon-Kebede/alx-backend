a2f6d5ccbbe2af364751f1c1b6f70fd01f77a2d5
Solomon Kebede <solomonaau188@gmail.com>
2023-03-03 14:54:30 +0000

    Task 9. Track progress and errors with Kue: Create the Job processor: 100%

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -1,27 +1,31 @@
 
- const kue = require('kue')
-let queue = kue.createQueue();
-let cluster = require('cluster');
-let clusterWorkerSize = 2;
+const kue = require('kue')
+const queue = kue.createQueue();
+//const cluster = require('cluster');
+//const clusterWorkerSize = 2;
 
-let blacklist = [4153518780, 4153518781];
+//let blacklist = [4153518780, 4153518781];
 
 queue.process('push_notification_code_2', (job, done) => {
-  let data = job.data;
-  let phoneNumber = data.phoneNumber;
-  let message = data.message;
+  const data = job.data;
+  const phoneNumber = data.phoneNumber;
+  const message = data.message;
   // console.log(job.data);
   sendNotification(phoneNumber, message, job, done);
-  done();
 })
 
 function sendNotification(phoneNumber, message, job, done) {
   job.progress(0, 100);
-  if (blacklist.includes(phoneNumber)) {
-    console.log(`Phone number ${phoneNumber} is blacklisted`);
-    done();
-  }
-  else {
+  const blacklist = ['4153518780', '4153518781'];
+  //console.log(typeof(phoneNumber));
+  //console.log(`${phoneNumber}  -> ${blacklist.includes(phoneNumber)}`);
+  const truthValue = blacklist.includes(phoneNumber);
+  if (truthValue === true) {
+  //if (phoneNumber == '4153518781' | phoneNumber == '4153518780') {
+    // console.log(`Phone number ${phoneNumber} is blacklisted`);
+    done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+  } else if (!truthValue) {
+  //else if (!blacklist.includes(phoneNumber)) {
     job.progress(50, 100);
     console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
     done();
